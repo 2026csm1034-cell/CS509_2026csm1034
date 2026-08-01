96cec1ea8fb388be6c8198d84d4181f97dac9628
Md Sadruzzama <sadruzzama@Mds-MacBook-Air.local>
2026-08-01 12:55:18 +0530

    source file changed

---
+++ assignment_01/src/gemm_simple.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+int main(){
+    int m , k, n;
+    cin>>m>>k>>n;
+    vector<vector<int>> mat1(m, vector<int>(k));
+    vector<vector<int>> mat2(k, vector<int>(n));
+    for(int i=0; i<m; i++){
+        for(int j=0; j<k; j++){
+            cin>>mat1[i][j];
+        }
+    }
+    for(int i=0; i<k; i++){
+        for(int j=0; j<n; j++){
+            cin>>mat2[i][j];
+        }
+    }
+
+    vector<vector<int>> final_mat(m, vector<int>(n,0));
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            for(int l=0; l<k; l++){
+                final_mat[i][j] += mat1[i][l] * mat2[l][j];
+            }
+        }
+    }
+    return 0;
+}
