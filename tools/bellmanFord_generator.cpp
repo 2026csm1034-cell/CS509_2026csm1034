e298a7b3cd62be37cde8d9658284bfb25ef907c5
Md Sadruzzama <sadruzzama@Mds-MacBook-Air.local>
2026-08-11 14:31:16 +0530

    All the test case is running

---
+++ tools/bellmanFord_generator.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <random>
+#include <set>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int sizes[] = {10, 100, 10000, 50000, 100000};
+
+    random_device rd;
+    mt19937 gen(rd());
+
+    uniform_int_distribution<int> weightDist(-2, 20);
+
+    for(int s = 0; s < 5; s++)
+    {
+        int V = sizes[s];
+        
+        // E = 3V
+        int E = 3 * V;
+
+        vector<vector<pair<int, int>>> graph(V);
+
+        set<pair<int, int>> usedEdges;
+
+        uniform_int_distribution<int> vertexDist(0, V - 1);
+
+        while((int)usedEdges.size() < E)
+        {
+            int u = vertexDist(gen);
+            int v = vertexDist(gen);
+
+            if(u == v)
+                continue;
+
+            if(usedEdges.count({u, v}))
+                continue;
+
+            usedEdges.insert({u, v});
+
+            int weight = weightDist(gen);
+
+            graph[u].push_back({v, weight});
+        }
+
+        string filename =
+            "bf_" + to_string(V) + ".txt";
+
+        ofstream file(filename);
+
+        if(!file)
+        {
+            cout << "Error creating "<< filename << endl;
+
+            return 1;
+        }
+
+        // V E
+        file << V << " " << E << "\n";
+
+        // adjacency list
+        for(int u = 0; u < V; u++)
+        {
+            file << u << " "<< graph[u].size();
+
+            for(auto edge : graph[u])
+            {
+                int v = edge.first;
+                int w = edge.second;
+
+                file << " "<< v << " "<< w;
+            }
+
+            file << "\n";
+        }
+
+        // Source
+        file << "SOURCE 0\n";
+
+        file.close();
+
+        cout << "Created "<< filename<< "  V=" << V<< " E=" << E<< endl;
+    }
+
+    return 0;
+}
---
+++ tools/floydWarshall_generator.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <fstream>
+#include <random>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    // Required test cases
+    int sizes[] = {
+        10,
+        100,
+        500,
+        1000,
+        2000
+    };
+
+    // Random number generator
+    random_device rd;
+    mt19937 gen(rd());
+
+    // Edge probability
+    // 30% chance of an edge
+    uniform_real_distribution<double> probability(0.0, 1.0);
+
+    // Positive edge weights
+    uniform_int_distribution<int> weightDist(-5, 20);
+
+    for(int s = 0; s < 5; s++)
+    {
+        int V = sizes[s];
+
+        string filename =
+            "fw_" + to_string(V) + ".txt";
+
+        ofstream file(filename);
+
+        if(!file)
+        {
+            cout << "Error creating "
+                 << filename << endl;
+
+            return 1;
+        }
+
+        // ----------------------------------
+        // First line = number of vertices
+        // ----------------------------------
+
+        file << V << "\n";
+
+        // ----------------------------------
+        // Generate V x V matrix
+        // ----------------------------------
+
+        for(int i = 0; i < V; i++)
+        {
+            for(int j = 0; j < V; j++)
+            {
+                // Diagonal must always be 0
+                if(i == j)
+                {
+                    file << "0";
+                }
+                else
+                {
+                    // 30% chance of an edge
+                    if(probability(gen) < 0.30)
+                    {
+                        int weight = weightDist(gen);
+
+                        file << weight;
+                    }
+                    else
+                    {
+                        file << "INF";
+                    }
+                }
+
+                // Space between entries
+                if(j < V - 1)
+                {
+                    file << " ";
+                }
+            }
+
+            file << "\n";
+        }
+
+        file.close();
+
+        cout << "Created "
+             << filename
+             << "  V = "
+             << V
+             << endl;
+    }
+
+    cout << "\nAll Floyd-Warshall test cases generated.\n";
+
+    return 0;
+}
